588060bc6fad425ec27384c6d9c3cfbe6198715e
deevj37378 <deev.jethwa@gmail.com>
2026-07-07 17:26:41 +0530

    js functionality added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const overlay = document.querySelector(".overlay");
 const authContainer = document.querySelector(".auth-container");
 
 overlay.addEventListener("click", () => {
+    overlay.classList.add("hidden");
     window.location.href = "index.html";
 });
 
